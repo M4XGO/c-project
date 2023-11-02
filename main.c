a5f91867bda357556e6d1d1c5f5e96fd073d084d
Maxime Nony <maximenony@MacBook-Pro-de-Maxime.local>
2023-11-02 14:19:39 +0100

    feat: reading function

--- main.c
+++ main.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+ 
+// Driver code
+int readFile()
+{
+    FILE* ptr;
+    char ch;
+ 
+    // Opening file in reading mode
+    ptr = fopen("./textFile/sas.txt", "r");
+ 
+    if (NULL == ptr) {
+        printf("file can't be opened \n");
+    }
+ 
+    printf("content of this file are \n");
+ 
+    // Printing what is written in file
+    // character by character using loop.
+    do {
+        ch = fgetc(ptr);
+        printf("%c", ch);
+ 
+        // Checking if character is not EOF.
+        // If it is EOF stop reading.
+    } while (ch != EOF);
+ 
+    // Closing the file
+    fclose(ptr);
+    return 0;
+}
 
-int main()
+
+int writeInFunction()
 {
     char *filename = "./textFile/sas.txt";
 
@@ -18,5 +51,13 @@ int main()
     // close the file
     fclose(fp);
 
+    return 0;
+}
+
+
+
+int main() {
+    writeInFunction();
+    readFile();
     return 0;
 }
